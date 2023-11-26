27435d5a7cec25d1199440c7bbc18495386c098f
LANSGANBS <2140967451@qq.com>
2023-11-26 10:06:48 +0800

    main

--- P4017 最大食物链计数.cpp
+++ P4017 最大食物链计数.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 void solve()
 {
-    
     // wout <<L" "<<endl;
     // cout << R"( )" << endl;
 }
--- test1.cpp
+++ test1.cpp
@@ -10,119 +10,29 @@ using namespace std;
 #define ll long long
 #define int ll
 
-// 堆排序实现
-void heapify(vector<int> &arr, int n, int i)
+void hannuota(int n, char begin, char end, char temp)
 {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-    if (largest != i)
+    if (n == 1)
     {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
+        cout << n << ": " << begin << " -> " << end << endl;
     }
-}
-void heap_sort(vector<int> &arr)
-{
-    int n = arr.size();
-
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    for (int i = n - 1; i > 0; i--)
+    else
     {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
+        hannuota(n - 1, begin, temp, end);
+        cout << n << ": " << begin << " -> " << end << endl;
+        hannuota(n - 1, temp, end, begin);
     }
 }
 
-// 计数排序辅助函数声明
-void counting_sort_by_digit(vector<int> &arr, int exp);
-// 计数排序实现
-void counting_sort(vector<int> &arr)
+void moveHanoiTower(int num, char begin, char temp, char end)
 {
-    int max_val = *max_element(arr.begin(), arr.end());
-    vector<int> count(max_val + 1, 0);
-    vector<int> output(arr.size());
-    for (int num : arr)
-        count[num]++;
-    for (int i = 1; i <= max_val; i++)
-        count[i] += count[i - 1];
-    for (int i = arr.size() - 1; i >= 0; i--)
-    {
-        output[count[arr[i]] - 1] = arr[i];
-        count[arr[i]]--;
-    }
-    arr = output;
-}
-
-// 基数排序实现
-void radix_sort(vector<int> &arr)
-{
-    int max_num = *max_element(arr.begin(), arr.end());
-    int exp = 1;
-
-    while (max_num / exp > 0)
-    {
-        counting_sort_by_digit(arr, exp);
-        exp *= 10;
-    }
-}
-void counting_sort_by_digit(vector<int> &arr, int exp)
-{
-    vector<int> output(arr.size());
-    vector<int> count(10, 0);
-
-    for (int i = 0; i < arr.size(); i++)
-        count[(arr[i] / exp) % 10]++;
-
-    for (int i = 1; i < 10; i++)
-        count[i] += count[i - 1];
-
-    for (int i = arr.size() - 1; i >= 0; i--)
-    {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-        count[(arr[i] / exp) % 10]--;
-    }
-    arr = output;
+    hannuota(num, begin, end, temp);
 }
 
 signed main()
 {
-    setlocale(LC_ALL, ""); // 设置本地环境，以支持宽字符
-
-    // 测试数组
-    vector<int> arr = {64, 25, 12, 22, 11};
-    wcout << L"原始数据: ";
-    for (int num : arr)
-        cout << num << " ";
-    cout << endl;
-
-    // 堆排序
-    vector<int> heap_sort_arr = arr;
-    heap_sort(heap_sort_arr);
-    wcout << L"堆排序结果: ";
-    for (int num : heap_sort_arr)
-        cout << num << " ";
-    cout << endl;
-
-    // 计数排序
-    vector<int> counting_sort_arr = arr;
-    counting_sort(counting_sort_arr);
-    wcout << L"计数排序结果: ";
-    for (int num : counting_sort_arr)
-        cout << num << " ";
-    cout << endl;
-
-    // 基数排序
-    vector<int> radix_sort_arr = arr;
-    radix_sort(radix_sort_arr);
-    wcout << L"基数排序结果: ";
-    for (int num : radix_sort_arr)
-        cout << num << " ";
-    cout << endl;
+    int num;
+    char begin, temp, end;
+    cin >> num >> begin >> temp >> end;
+    moveHanoiTower(num, begin, temp, end);
 }
